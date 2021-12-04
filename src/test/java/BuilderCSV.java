874d89cfe1e16aa4e0834fa025232ae3c4489ba4
QA_OFlavio <flavio.craveiro.oliveira@devoteam.com>
2021-12-04 12:32:16 +0000

    COMMIT repository

--- src/test/java/BuilderCSV.java
+++ src/test/java/BuilderCSV.java
@@ -12,7 +12,7 @@ public class BuilderCSV {
 
 
     public static void main(String[] args) {
-        givenDataArray_whenConvertToCSV_thenOutputCreated();
+        //givenDataArray_whenConvertToCSV_thenOutputCreated();
 
     }
 
