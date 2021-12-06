8320fa5148baeb08eb4d14219789fe8ccc791e66
QA_OFlavio <flavio.craveiro.oliveira@devoteam.com>
2021-12-06 14:12:00 +0000

    COMMIT repository
    Alpha release V0612.21
    
    STATUS:
    CSV CREATOR (XRAY LAYOUT SAMPLE)
    API GET EXAMPLES
    API POST EXAMPLES
    API POST METHOD USING JSON FILES

--- src/test/java/APITestSample.java
+++ src/test/java/APITestSample.java
@@ -1,53 +1,132 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 import static io.restassured.RestAssured.given;
 
 public class APITestSample {
 
-    //Executado ANTES de cada TESTE (Não Obrigatório!)
-    //@BeforeTest
+    //Executado ANTES da classe (Não Obrigatório!)
+    @BeforeClass
+    public static void setup() {
+        String baseHost = System.getProperty("server.host");
+        if(baseHost==null){
+            baseHost = "https://jsonplaceholder.typicode.com";
+        }
+        RestAssured.baseURI = baseHost;
 
-    //Executado DEPOIS de cada TESTE (Não Obrigatório!)
-    //@AfterTest
+        //Name the CSV File to Create
+        String FileName = "TestGET";
+        BuilderCSV builderCSV = new BuilderCSV();
+        builderCSV.setFileName(FileName);
+    }
 
-    //Tests Scripts
-    @Test (priority = 0)
-    void test_GET_SAMPLE_PASS(){
+    //Test Scripts
+    @Test(priority = 0)
+    void GET_SAMPLE_PASS() throws IOException {
+        //Define Endpoint
+        String ENDPOINT = "/posts/1/comments";
+
         Response response = given()
-                .queryParam("CUSTOMER_ID","68195") //queryParam Não  é Obrigatorio !
+                .queryParam("CUSTOMER_ID","68195") //queryParam Não é Obrigatorio !
                 .queryParam("PASSWORD","1234!")
                 .queryParam("Account_No","1") //****
                 .when()
-                .get("http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1")
+                .get(ENDPOINT)
                 .then()
-                .log().body()
+                .log().all()
                 .extract().response();
 
+        //Write to CSV
+        String TestId = "01";
+        String Summary = "test_GET_SAMPLE_PASS";
+        String Description = "ESTE TESTE É UM GET QUE VAI PASSAR COM STATUS CODE 200";
+        String Action = "Test GET methot trough endpoint "+ENDPOINT;
+        String ExpectedResult = "Status code: HTTP/1.1."+"200";
+        String Data = "Validate if API ENDPOINT is getting the right status code";
+
+        BuilderCSV builderCSV = new BuilderCSV();
+        builderCSV.setTestId(TestId);
+        builderCSV.setSummary(Summary);
+        builderCSV.setDescription(Description);
+        builderCSV.setAction(Action);
+        builderCSV.setExpectedResult(ExpectedResult);
+        builderCSV.setData(Data);
+
+        BuilderCSV.main();
+        //****
+
         Assert.assertEquals(response.getStatusCode(), 200);
-        Assert.assertEquals(response.getBody().asString().contains("TRANSACTION_ID") /*Expected value*/, true /*Actual Value*/);
+        Assert.assertTrue(response.getBody().asString().contains("odio adipisci rerum aut animi"));
 
     }
 
     @Test(priority = 1)
-    void test2_GET_SAMPLE_FAIL(){
+    void GET_SAMPLE_FAIL() throws IOException {
+        //Define Endpoint
+        String ENDPOINT = "/posts/1/comments";
+
         Response response = given()
                 .queryParam("CUSTOMER_ID","68195") //queryParam Não  é Obrigatorio !
                 .queryParam("PASSWORD","1234!")
                 .queryParam("Account_No","1") //****
                 .when()
-                .get("http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1")
+                .get(ENDPOINT)
+                .then()
+                .log().body()
+                .extract().response();
+
+        //Write to CSV
+        String TestId = "02";
+        String Summary = "test_GET_SAMPLE_FAIL";
+        String Description = "02";
+        String Action = "Test GET methot trough endpoint"+ENDPOINT;
+        String ExpectedResult = "Status code: HTTP/1.1."+"200";
+        String Data = "02";
+
+        BuilderCSV builderCSV = new BuilderCSV();
+        builderCSV.setTestId(TestId);
+        builderCSV.setSummary(Summary);
+        builderCSV.setDescription(Description);
+        builderCSV.setAction(Action);
+        builderCSV.setExpectedResult(ExpectedResult);
+        builderCSV.setData(Data);
+
+        BuilderCSV.main();
+        //****
+
+        Assert.assertEquals(response.getStatusCode(), 201);
+        Assert.assertTrue(response.getBody().asString().contains("architecto"));
+    }
+
+    @Test(priority = 2)
+    public void POST_SAMPLE_PASS() throws IOException, ParseException {
+        //Define Endpoint
+        String ENDPOINT = "/posts";
+
+        //CALL DEFINED JSON
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("src/test/java/JSONS_REQUESTS/test_POST_SAMPLE_PASS.json"));
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(obj)
+                .when()
+                .post(ENDPOINT)
                 .then()
                 .log().body()
                 .extract().response();
 
         Assert.assertEquals(response.getStatusCode(), 201);
-        Assert.assertEquals(response.getBody().asString().contains("TRANSACTION_ID") /*Expected value*/, true /*Actual Value*/);
 
-        
     }
+
 }
--- src/test/java/BuilderCSV.java
+++ src/test/java/BuilderCSV.java
@@ -1,50 +1,75 @@
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import com.opencsv.CSVWriter;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.testng.AssertJUnit.assertTrue;
 
 public class BuilderCSV {
 
+    private static String FileName;
+    private static String TestId;
+    private static String Summary;
+    private static String Description;
+    private static String Action;
+    private static String ExpectedResult;
+    private static String Data;
 
-    public static void main(String[] args) {
-        //givenDataArray_whenConvertToCSV_thenOutputCreated();
+    public static void main() throws IOException {
 
-    }
+        String CSVName = FileName;
+        if (CSVName == null || CSVName == "" ) {
+            System.out.println("\n***************************************");
+            System.out.println("INFO STATUS: FileName MUST be declared !!");
+            System.out.println("***************************************");
+            System.exit(0);
 
+        }else{
 
-    public void givenDataArray_whenConvertToCSV_thenOutputCreated() throws IOException {
-        List<String[]> dataLines  = new ArrayList<>();
-        dataLines .add(new String[]{"John", "Doe", "38", "Comment Data\nAnother line of comment data"});
-        dataLines .add(new String[]{"Jane", "Doe, Jr.", "19", "She said \"I'm being quoted\""});
+            List<String[]> csvData = createCsvDataSimple(TestId, Summary, Description, Action, ExpectedResult, Data);
+            try (CSVWriter writer = new CSVWriter(new FileWriter("src/test/java/BuilderCSV/"+CSVName+".csv",true))) {
+                writer.writeAll(csvData);
+            }
 
-        File csvOutputFile = new File("CSV_FILE_NAME");
-        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-            dataLines.stream()
-                    .map(this::convertToCSV)
-                    .forEach(pw::println);
         }
-        assertTrue(csvOutputFile.exists());
+
     }
 
-    public String convertToCSV(String[] data) {
-        return Stream.of(data)
-                .map(this::escapeSpecialCharacters)
-                .collect(Collectors.joining(","));
+    private static List<String[]> createCsvDataSimple(String TestId, String Summary, String Description, String Action, String ExpectedResult, String Data) {
+        List<String[]> list = new ArrayList<>();
+        String[] header = {"TestId", "TestType", "Summary", "Description", "Action", "ExpectedResult", "Data", "Label"};
+        list.add(header);
+
+            String[] record = {TestId, "API",Summary, Description, Action, ExpectedResult, Data, "API_TEST"};
+            list.add(record);
+
+        return list;
     }
 
-    public String escapeSpecialCharacters(String data) {
-        String escapedData = data.replaceAll("\\R", " ");
-        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
-            data = data.replace("\"", "\"\"");
-            escapedData = "\"" + data + "\"";
-        }
-        return escapedData;
+    public void setFileName(String fileName) {
+        FileName = fileName;
     }
 
+    public void setTestId(String testId) {
+        TestId = testId;
+    }
 
+    public void setSummary(String summary) {
+        Summary = summary;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public void setAction(String action) {
+        Action = action;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        ExpectedResult = expectedResult;
+    }
+
+    public void setData(String data) {
+        Data = data;
+    }
 }
